984163b957fb5b2f3a1f2ae2e4b0f1a4f375ed91
Juan Tenezaca <tenj34@gmail.com>
2018-01-05 20:07:26 -0500

    first comm
    We are able to create meme objects. We are able to retrieve them from core data

---
+++ MemeCollector/MemeViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MemeViewController.swift
+//  MemeCollector
+//
+//  Created by Juan Tenezaca on 11/26/17.
+//  Copyright © 2017 Juan Tenezaca. All rights reserved.
+//
+
+import UIKit
+
+class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        memeImageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let meme = Meme(context: context)
+        meme.title = titleTextField.text
+        meme.image = UIImagePNGRepresentation(memeImageView.image!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+    }
+    
+}
--- MemeCollector/ViewController.swift
+++ MemeCollector/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+   // @IBOutlet weak var tableView: UITableView!
+    
+    
+    var memes : [Meme] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do{
+            memes = try context.fetch(Meme.fetchRequest())
+            tableView.reloadData()
+        } catch {
+            
+        }
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = UITableViewCell()
+        let meme  = memes[indexPath.row]
+        cell.textLabel?.text = meme.title
+        cell.imageView?.image = UIImage(data : meme.image as! Data)
+        return cell
+    }
 }
 
